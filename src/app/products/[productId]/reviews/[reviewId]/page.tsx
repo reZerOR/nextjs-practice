cf58527020ad3faba4c28b1c5b7b9413c8e2d046
reZerOR <gkraihan14k@gmail.com>
2024-01-12 17:17:07 +0600

    custom 404 page

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,8 +1,13 @@
+import { notFound } from "next/navigation";
+
 export default function ReviewDetails({
   params,
 }: {
   params: { productId: string; reviewId: string };
 }) {
+  if (parseInt(params.reviewId) > 1000) {
+    notFound();
+  }
   return (
     <div className="flex min-h-screen items-center text-7xl font-bold justify-center">
       <h1>
