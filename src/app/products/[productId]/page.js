7a1b984e1875a38ed929a36c7f366fdb68617e20
reZerOR <gkraihan14k@gmail.com>
2024-01-12 11:54:44 +0600

    Dynamic Routes

---
+++ src/app/products/[productId]/page.js
@@ -0,0 +1,7 @@
+export default function ProductId({params}) {
+  return (
+<div className='flex min-h-screen items-center text-7xl font-bold justify-center'>
+  <h1>Product {params.productId} page</h1>
+</div>
+  )
+}
---
+++ src/app/products/page.js
@@ -0,0 +1,10 @@
+export default function Products() {
+  return (
+<div className='flex flex-col gap-6 min-h-screen items-center text-7xl font-bold justify-center'>
+  <h1>Products page</h1>
+  <h2>products 1</h2>
+  <h2>products 2</h2>
+  <h2>products 3</h2>
+</div>
+  )
+}
