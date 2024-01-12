4b0aac9d5d29ebd3795621e04c908afcbb649255
reZerOR <gkraihan14k@gmail.com>
2024-01-12 21:10:51 +0600

    Route groups

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,7 @@
+export default function Login() {
+  return (
+    <div className="flex min-h-screen items-center text-7xl font-bold justify-center">
+      <h2>Login page</h2>
+    </div>
+  );
+}
---
+++ src/app/(auth)/register/page.tsx
@@ -0,0 +1,7 @@
+export default function Register() {
+  return (
+    <div className="flex min-h-screen items-center text-7xl font-bold justify-center">
+      <h2>Register page</h2>
+    </div>
+  );
+}
