d9631c4d74cc57b61d8b58341cda4d96ce93ab90
reZerOR <gkraihan14k@gmail.com>
2024-01-12 20:57:38 +0600

    private route

---
+++ src/app/_lib/page.tsx
@@ -0,0 +1,7 @@
+export default function PrivateRoute() {
+  return (
+    <div className="flex min-h-screen items-center text-7xl font-bold justify-center">
+      <h2>this not gonna show to browsers cause this is a private route</h2>
+    </div>
+  );
+}
