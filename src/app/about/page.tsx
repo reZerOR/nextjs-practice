d69079c55e76f0dbc55b880179d3e238d69dc5e8
reZerOR <gkraihan14k@gmail.com>
2024-01-13 11:55:38 +0600

    meta data and dynamic meta data

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,11 @@
+export const metadata = {
+  title: "About",
+};
+
 export default function About() {
   return (
-<div className='flex min-h-screen items-center text-7xl font-bold justify-center'>
-  <h1>About page</h1>
-</div>
-  )
+    <div className="flex min-h-screen items-center text-7xl font-bold justify-center">
+      <h1>About page</h1>
+    </div>
+  );
 }
--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,8 +1,25 @@
-export default function ProductId({
+import { Metadata } from "next";
+
+type Props = {
+  params: {
+    productId: string;
+  };
+};
+
+export const generateMetadata = async ({
   params,
-}: {
-  params: { productId: string };
-}) {
+}: Props): Promise<Metadata> => {
+  const title = await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(`Iphone ${params.productId}`);
+    }, 100);
+  });
+  return {
+    title: `Product ${title}`,
+  };
+};
+
+export default function ProductId({ params }: Props) {
   return (
     <div className="flex min-h-screen items-center text-7xl font-bold justify-center">
       <h1>Product {params.productId} Details page</h1>
