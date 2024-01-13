811c18a0ad69ac01e7631a1ebe3d604532dc9a5c
reZerOR <gkraihan14k@gmail.com>
2024-01-13 15:43:07 +0600

    navigation through links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,21 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-<div className='flex min-h-screen items-center text-7xl font-bold justify-center'>
-  <h1>Home page</h1>
-</div>
-  )
+    <div className="flex flex-col min-h-screen items-center text-7xl font-bold justify-center">
+      <h1>Home page</h1>
+      <div className="space-x-3">
+        <Link href={"/blog"}>
+          <button className="text-base py-3 mt-4 px-8 bg-amber-500 rounded-lg">
+            Blog
+          </button>
+        </Link>
+        <Link href={"/products"}>
+          <button className="text-base py-3 mt-4 px-8 bg-amber-500 rounded-lg">
+            products
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
 }
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,10 +1,17 @@
+import Link from "next/link";
+
 export default function Products() {
+  const links = [1, 2, 3];
   return (
-<div className='flex flex-col gap-6 min-h-screen items-center text-7xl font-bold justify-center'>
-  <h1>Products page</h1>
-  <h2>products 1</h2>
-  <h2>products 2</h2>
-  <h2>products 3</h2>
-</div>
-  )
+    <div className="flex flex-col gap-6 min-h-screen items-center text-7xl font-bold justify-center">
+      <h1>Products page</h1>
+      {links.map((item, index) => (
+        <Link key={index} href={`/products/${item}`}>
+          <button className="text-base py-3 mt-4 px-8 bg-amber-500 rounded-lg">
+            Product {item}
+          </button>
+        </Link>
+      ))}
+    </div>
+  );
 }
