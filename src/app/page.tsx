d911008b144555227942832b3bc480a2337b3262
reZerOR <gkraihan14k@gmail.com>
2024-02-03 19:23:28 +0600

    svg animation and svg manupulation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import SvgColorChange from "@/components/SvgColorChange";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen items-center text-7xl font-bold justify-center">
       <h1>Home page</h1>
+      <SvgColorChange></SvgColorChange>
       <div className="space-x-3">
         <Link href={"/blog"}>
           <button className="text-base py-3 mt-4 px-8 bg-amber-500 rounded-lg">
